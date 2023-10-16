99ce5e0c85d7ad97b033d5a89445d313c995571c
RomeoPrtl <88932199+RomeoPrtl@users.noreply.github.com>
2023-10-16 17:32:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,63 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
-    <title>Enregistrement d'un objectif</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AMAZOFF</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-
-<a href="index.html">Accueil</a>
-
-<?php
-// Connexion à la base de données
-$serveur = "localhost";
-$utilisateur = "root";
-$mot_de_passe = "";
-$base_de_donnees = "db_onegoal";
-
-$connexion = new mysqli($serveur, $utilisateur, $mot_de_passe, $base_de_donnees);
-
-if ($connexion->connect_error) {
-    die("Échec de la connexion : " . $connexion->connect_error);
-} else {
-    echo "Connexion réussie 😀<br>";
-}
-
-// Récupérer les données en les échappant
-$titre = mysqli_real_escape_string($connexion, $_GET['titreObj']);
-$date_deb = mysqli_real_escape_string($connexion, $_GET['dateObjDeb']);
-$date_fin = mysqli_real_escape_string($connexion, $_GET['dateObjFin']);
-
-// Requête préparée
-$req = $connexion->prepare("INSERT INTO objectif (nom_objectif, date_deb_obj, date_fin_obj) VALUES (?, ?, ?)");
-$req->bind_param("sss", $titre, $date_deb, $date_fin);
-
-if ($req->execute()) {
-    echo "Enregistrement réussi.";
-} else {
-    echo "Erreur d'enregistrement : " . $req->error;
-}
-
-// Fermer la connexion
-$connexion->close();
-?>
-
+    <header>
+        <nav>
+            <h1>Amazoff</h1>
+            <ul>
+                <li><button><a href="index.php">Accueil</a></button></li>
+                <li><button><a href="panier.php">Mon panier</a></button></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h2>Nos produits : </h2>
+        <div class="container">
+            <ul>
+                <li>
+                    <h3>Produit A</h3>
+                    <p>9 euros</p>
+                    <button><a href="panier.php?nom=Produit-A">Ajouter au panier</a></button>
+                </li>
+                <li>
+                    <h3>Produit B</h3>
+                    <p>19 euros</p>
+                    <button><a href="panier.php?nom=Produit-B">Ajouter au panier</a></button>
+                </li>
+                <li>
+                    <h3>Produit C</h3>
+                    <p>79 euros</p>
+                    <button><a href="panier.php?nom=Produit-C">Ajouter au panier</a></button>
+                </li>
+                <li>
+                    <h3>Produit D</h3>
+                    <p>5 euros</p>
+                    <button><a href="panier.php?nom=Produit-D">Ajouter au panier</a></button>
+                </li>
+                <li>
+                    <h3>Produit E</h3>
+                    <p>45 euros</p>
+                    <button><a href="panier.php?nom=Produit-E">Ajouter au panier</a></button>
+                </li>
+                <li>
+                    <h3>Produit F</h3>
+                    <p>29 euros</p>
+                    <button><a href="panier.php?nom=Produit-F">Ajouter au panier</a></button>
+                </li>
+                <li>
+                    <h3>Produit G</h3>
+                    <p>10 euros</p>
+                    <button><a href="panier.php?nom=Produit-G">Ajouter au panier</a></button>
+                </li>
+            </ul>
+        </div>
+    </main>
 </body>
 </html>
